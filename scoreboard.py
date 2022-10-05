be49b30e0f43fb0bcdde7e761c29ebbc8a002378
Marco <fernstaedtmarco@gmail.com>
2022-10-05 15:58:26 -0700

    scoreboard is now white and ppears at the top center of screen

--- scoreboard.py
+++ scoreboard.py
@@ -5,4 +5,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.color('white')
+        self.penup()
+        self.goto(x=0, y=270)
         self.write(f"Score: {self.score}", align='center', font=("Arial", 24, "normal"))
+        self.hideturtle()
