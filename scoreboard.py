608e8d72035fa3c7ae49f7b746b0d1adb3386692
Marco <fernstaedtmarco@gmail.com>
2022-10-05 16:07:50 -0700

    added constant variables for text alignment and font built a method to update the scoreboard and to increase score

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,8 @@
+from ctypes import alignment
 from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
 
 class Scoreboard(Turtle):
 
@@ -8,5 +12,13 @@ class Scoreboard(Turtle):
         self.color('white')
         self.penup()
         self.goto(x=0, y=270)
-        self.write(f"Score: {self.score}", align='center', font=("Arial", 24, "normal"))
         self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
