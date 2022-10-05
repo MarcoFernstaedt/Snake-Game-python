a01f88e290dd2272f10003a3be92a94defb4d223
Marco <fernstaedtmarco@gmail.com>
2022-10-05 14:29:03 -0700

    made snake file to store snake object

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,16 +8,7 @@ screen.bgcolor('black')
 screen.title("Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-for position in starting_positions:
-    new_segment = Turtle(shape='square')
-    new_segment.color('white')
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
 game_is_on = True
 
@@ -24,10 +16,6 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num -1].xcor()
-        new_y = segments[seg_num -1].ycor()
-        segments[seg_num].goto(x=new_x, y=new_y)
-    segments[0].fd(20)
+    snake.move()
 
 screen.exitonclick()
